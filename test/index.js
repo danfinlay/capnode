5715a8943f682f2fdfa5c9f49d8493ab7c6ca153
Dan Finlay <dan@danfinlay.com>
2019-02-07 07:02:06 -0800

    Add additional good test for bidirectional, commented out

--- test/index.js
+++ test/index.js
@@ -48,10 +48,10 @@ test('a method in an object', (t) => {
   client.pipe(server).pipe(client)
 })
 
-
+/*
 test('ability to return additional promise-returning functions', (t) => {
   const server = capnode.create({
-    foo: () => Promise.resolve({
+    foo: (cb) => Promise.resolve({
       bam: (arg) => Promise.resolve('baz' + arg)
     }),
   })
@@ -61,8 +61,8 @@ test('ability to return additional promise-returning functions', (t) => {
   client.on('remote', async (remote) => {
 
     t.equal(remote.bam, 'baz', 'remote returned concrete value.')
-    const result = await remote.foo()
-    t.equal(typeof result, 'object', 'returned an object as expected')
+    const result = await remote.foo(update)
+   t.equal(typeof result, 'object', 'returned an object as expected')
     t.equal(typeof resut.bam, 'function', 'object has a function named bam')
     const result2 = await result.bam('!')
     t.equal(result2, 'baz!', 'Remote operated on nested method.')
@@ -71,6 +71,35 @@ test('ability to return additional promise-returning functions', (t) => {
 
   client.pipe(server).pipe(client)
 })
+*/
 
-test('ability to pass promiseResolving func back to server')
+/*
+test('ability to pass promiseResolving func back to server', (t) => {
+  const server = capnode.create({
+    foo: async (emit) => {
+      emit('update')
+      emit('update')
+      return Promise.resolve(true)
+    }
+  })
 
+  const client = capnode.connect(server)
+
+  client.on('remote', async (remote) => {
+
+    t.equal(remote.bam, 'baz', 'remote returned concrete value.')
+    const result = await remote.foo(update)
+
+    let callCount = 0
+    async function update (arg) {
+      t.equal(arg, 'update', 'client could send fn up to server')
+      if (++callCount === 2) {
+        t.end()
+      }
+    }
+  })
+
+  client.pipe(server).pipe(client)
+})
+
+*/
