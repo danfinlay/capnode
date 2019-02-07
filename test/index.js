231f931c10e0ee1e32c1d0fcadd51be4eda2d9dc
Dan Finlay <dan@danfinlay.com>
2019-02-06 16:42:28 -0800

    Added test for recursive method calling

--- test/index.js
+++ test/index.js
@@ -21,3 +21,26 @@ test('a basic connection', (t) => {
   client.pipe(server).pipe(client)
 })
 
+test('ability to return additional promise-returning functions', (t) => {
+  const server = capnode.createServer({
+    foo: () => Promise.resolve({
+      bam: (arg) => Promise.resolve('baz' + arg)
+    }),
+  })
+
+  const client = capnode.createClient(server)
+
+  client.on('remote', async (remote) => {
+
+    t.equal(remote.bam, 'baz', 'remote returned concrete value.')
+    const result = await remote.foo()
+    t.equal(typeof result, 'object', 'returned an object as expected')
+    t.equal(typeof resut.bam, 'function', 'object has a function named bam')
+    const result2 = await result.bam('!')
+    t.equal(result2, 'baz!', 'Remote operated on nested method.')
+    t.end()
+  })
+
+  client.pipe(server).pipe(client)
+})
+
