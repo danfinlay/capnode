7000f1055c9f86a649fe396e88e389202ab00b07
Dan Finlay <dan@danfinlay.com>
2019-07-17 22:56:51 -0700

    Fix stream behavior

--- src/remote.ts
+++ src/remote.ts
@@ -19,6 +19,9 @@ export default class Remote extends Duplex {
     super({
       objectMode: true,
       write: (message: ICapnodeMessage, _encoding, cb: (err?: Error) => void) => {
+        if (!message) {
+          return cb();
+        }
         try {
           this.receiveMessage(message);
         } catch (err) {
@@ -30,26 +33,26 @@ export default class Remote extends Duplex {
       read: () => {
         this.streamReading = true;
 
-        // recipient is ready to have messages pushed.
-        if (this.queue.length > 0) {
-          let next = this.queue.shift()
+        const queue = this.queue;
+        if (queue.length > 0) {
+          let next = queue.shift()
           while (this.push(next)) {
-            next = this.queue.shift()
+            next = queue.shift();
           }
 
-          if (this.queue.length > 0) {
-            // Recipient is overloaded, resume queueing:
+          if (queue.length > 0) {
             this.streamReading = false
           }
         }
       }
     });
 
+    // Connect stream to remote listeners:
     this.sendMessageOverStream = this.sendMessageOverStream.bind(this);
     this.addRemoteMessageListener(this.sendMessageOverStream);
 
     if (messageHandler) {
-      this.addMessageListener(messageHandler);
+      this.addLocalMessageListener(messageHandler);
     }
   }
 
@@ -65,7 +68,7 @@ export default class Remote extends Duplex {
    * @param sender - A Function that will deliver the message to the local capnode.
    * Called when the Remote is constructed within a capnode.
    */
-  addMessageListener (sender?: ICapnodeMessageSender) {
+  addLocalMessageListener (sender?: ICapnodeMessageSender) {
     if (!sender) return;
     this.localMessageListeners.add(sender);
   }
@@ -94,10 +97,12 @@ export default class Remote extends Duplex {
   }
 
   receiveMessage(message: ICapnodeMessage): void {
+    if (!message) return;
     this.informAll(this.localMessageListeners, message);
   }
 
   emitMessage(message: ICapnodeMessage): void {
+    if (!message) return;
     this.informAll(this.remoteMessageListeners, message);
   }
 }
